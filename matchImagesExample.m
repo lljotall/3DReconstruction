3a7fb12591a9b8ae82b0d9eb63ce81e6178fcdc6
gondimribeiro <gondimribeiro@gmail.com>
2015-06-07 11:47:57 -0300

    matchImagesExample.m changed to better visualization

--- matchImagesExample.m
+++ matchImagesExample.m
@@ -1,12 +1,20 @@
 % Read the stereo images.
-I1 = imread('tom/tom_100_25__0_0.tiff');
-I2 = imread('tom/tom_100_25__0_5.tiff');
+I1 = imread('tom/tom_100_25__0_5.tiff');
+I2 = imread('tom/tom_100_25__0_8.tiff');
 
 % Match images
 [ matchedPoints1, matchedPoints2, metric ] = matchImages( I1, I2, 'SURF' );
 
 % Visualize the corresponding points.
-figure; showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
-
-% Prints their coordinates in I1
-matchedPoints1.Location
+coord = matchedPoints1.Location
+subplot(1, 2, 1); 
+imshow(I1);
+hold on;
+scatter(coord(:, 1), coord(:, 2), 5);
+hold off;
+coord = matchedPoints2.Location;
+subplot(1, 2, 2);
+imshow(I2);
+hold on;
+scatter(coord(:, 1), coord(:, 2), 5);
+hold off;
